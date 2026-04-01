37947307ac57045c2260b9cde8544cac84b0ac7e
sirgdwrd <147624407+sirgdwrd@users.noreply.github.com>
2026-04-01 11:42:50 +0800

    fix issye

--- app/src/main/java/com/example/academicleveling/ui/profile/PlayerProgressScreen.kt
+++ app/src/main/java/com/example/academicleveling/ui/profile/PlayerProgressScreen.kt
@@ -1,8 +1,8 @@
 package com.example.academicleveling.ui.profile
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.image.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -30,13 +30,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.graphics.Color
-<<<<<<< Updated upstream
 import androidx.compose.ui.graphics.vector.ImageVector
-=======
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
->>>>>>> Stashed changes
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
